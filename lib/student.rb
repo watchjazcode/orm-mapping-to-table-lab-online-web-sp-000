959888fbe6edee4b93f781da1142db16485d40c9
Jazmin M <jazminmerckel@gmail.com>
2019-12-09 05:26:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name:, grade:, id: nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
